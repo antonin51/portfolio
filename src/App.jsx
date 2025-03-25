61d11dffa4fb0d35b9e652615e909892f6252cb3
antonin51 <antonin.doliwa1@gmail.com>
2025-03-25 16:21:08 +0100

    correction bug counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import 'typeface-inter'
 import Portfolio from './components/portfolio'
 import About from './pages/about'
 import Book from './pages/book'
-import CounterPage from './pages/counter'
 import Erreur from './pages/Erreur'
 import Exercice from './pages/exercice'
 import Home from './pages/home'
@@ -20,7 +19,7 @@ function App() {
         <Route path="/book" element={<Book />} />
         <Route path='/exercice' element={<Exercice />} />
         <Route path='/portfolio' element={<Portfolio />} />
-        <Route path='/counter' element={<CounterPage />} />
+        {/* <Route path='/counter' element={<CounterPage />} /> */}
         <Route path='*' element={<Erreur />} />
       </Routes>
     </Router>
